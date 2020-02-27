90e89ed25d478e04cb563656936dcd43af4306e2
Patrick the Ratrick <keavenyp@gmail.com>
2020-02-27 11:04:34 -0700

    Create data.const.js

---
+++ data.const.js
@@ -0,0 +1,98 @@
+const DATA = [{
+		id: 1,
+		name: 'AirSense 10 AutoSet for Her CPAP Machine',
+		description: "<p>Fitting 99% of faces worldwide, AirFit N20&rsquo;s blend of intuitive design and comfort-driven features is designed to deliver exceptional performance in a nasal mask.&nbsp;The InfinitySeal cushion technology is the most adaptive silicone cushion New England Medical Supply has ever designed. It features specially-designed loops and varying thickness levels designed to prevent blowout.&nbsp;The AirFit N20 was designed to be easy to put on and use that you don&rsquo;t necessarily need specific fitting instructions. The simple, modular design and magnetic clips guide the mask&rsquo;s headgear to its frame quickly and easily. The AirFit N20 comfortably moves with you throughout the night, regardless of sleep position.&nbsp;</p>\n<p></p>\n<p>- Innovative InfinitySeal silicone cushion designed to fit everyone, tolerate misfitting, and accommodate patient movement</p>\n<p>-&nbsp;\No rules fit\ mask that is simple to put on and fit without sacrificing performance</p>\n<p>- The specially designed silicone cushion creates a secure seal and prevents mask leaks</p>\n<p>- Plush-padded frame provides comfort throughout the night</p>\n<p>- Ideal for those who breathe through their nose during sleep</p>\n<p></p>\n<p>- Easy-to-use magnetic clips for quick fitting</p>\n<p>- Clear field of vision for patients who read or watch TV in bed</p>",
+		price: 22.0,
+		currency: USD,
+		in_stock: true,
+		vendor: {
+			id: 30,
+			name: 'New England Medical Supply'
+		}
+	},
+	{
+		id: 2,
+		name: 'Opus 360 Nasal Pillow CPAP Mask with Headgear',
+		description: "<p>Fitting 99% of faces worldwide, AirFit N20&rsquo;s blend of intuitive design and comfort-driven features is designed to deliver exceptional performance in a nasal mask.&nbsp;The InfinitySeal cushion technology is the most adaptive silicone cushion New England Medical Supply has ever designed. It features specially-designed loops and varying thickness levels designed to prevent blowout.&nbsp;The AirFit N20 was designed to be easy to put on and use that you don&rsquo;t necessarily need specific fitting instructions. The simple, modular design and magnetic clips guide the mask&rsquo;s headgear to its frame quickly and easily. The AirFit N20 comfortably moves with you throughout the night, regardless of sleep position.&nbsp;</p>\n<p></p>\n<p>- Innovative InfinitySeal silicone cushion designed to fit everyone, tolerate misfitting, and accommodate patient movement</p>\n<p>-&nbsp;\No rules fit\ mask that is simple to put on and fit without sacrificing performance</p>\n<p>- The specially designed silicone cushion creates a secure seal and prevents mask leaks</p>\n<p>- Plush-padded frame provides comfort throughout the night</p>\n<p>- Ideal for those who breathe through their nose during sleep</p>\n<p></p>\n<p>- Easy-to-use magnetic clips for quick fitting</p>\n<p>- Clear field of vision for patients who read or watch TV in bed</p>",
+		price: 12.0,
+		currency: USD,
+		in_stock: true,
+		vendor: {
+			id: 30,
+			name: 'New England Medical Supply'
+		}
+	},
+	{
+		id: 3,
+		name: 'AirFit F20 Full Face CPAP Mask with Headgear',
+		description: "<p>Specially designed to fit every face with ease regardless of facial profile, New England Medical Supply's AirFit F20 combines innovative cushion design with easy-to-use features to deliver an outstanding experience for full face mask users.&nbsp;International studies found that the AirFit F20 successfully fit 96.5% of all patients. Tested to comfortably fit various face shapes and sizes, and proven to perform at high therapy pressures, the AirFit F20 is a mask that caters to your wide range of patients and their needs.&nbsp;</p><p>-&nbsp;The InfinitySeal&trade; cushion technology features specially-designed &ldquo;infinity&rdquo; loops and varying thickness levels designed to reduce blowout and provide support at the sides of the nose. The silicone cushion is designed to also tolerate misfitting and accommodate patient movement.&nbsp;</p><p>- Comfortable and flexible frame designed to support the widest range of facial types</p><p>- With its modular design, each size cushion can easily be clicked into the standard AirFit F20 mask frame, helping streamline patient setups.&nbsp;Magnetic clips are easy to use for quick fitting</p><p>-&nbsp;Designed to perform across a wide range of therapy pressures</p><p>-&nbsp;Modular frame fits all cushion sizes</p><p>- Clear field of vision for reading or watching TV in bed</p>",
+		price: 32.0,
+		currency: USD,
+		in_stock: true,
+		vendor: {
+			id: 30,
+			name: 'New England Medical Supply'
+		}
+	},
+	{
+		id: 4,
+		name: 'AirFit F20 Full Face CPAP Mask with Headgear',
+		description: "<p>Specially designed to fit every face with ease regardless of facial profile, Cape's AirFit F20 combines innovative cushion design with easy-to-use features to deliver an outstanding experience for full face mask users.&nbsp;International studies found that the AirFit F20 successfully fit 96.5% of all patients. Tested to comfortably fit various face shapes and sizes, and proven to perform at high therapy pressures, the AirFit F20 is a mask that caters to your wide range of patients and their needs.&nbsp;</p><p>-&nbsp;The InfinitySeal&trade; cushion technology features specially-designed &ldquo;infinity&rdquo; loops and varying thickness levels designed to reduce blowout and provide support at the sides of the nose. The silicone cushion is designed to also tolerate misfitting and accommodate patient movement.&nbsp;</p><p>- Comfortable and flexible frame designed to support the widest range of facial types</p><p>- With its modular design, each size cushion can easily be clicked into the standard AirFit F20 mask frame, helping streamline patient setups.&nbsp;Magnetic clips are easy to use for quick fitting</p><p>-&nbsp;Designed to perform across a wide range of therapy pressures</p><p>-&nbsp;Modular frame fits all cushion sizes</p><p>- Clear field of vision for reading or watching TV in bed</p>",
+		price: 10.0,
+		currency: USD,
+		in_stock: true,
+		vendor: {
+			id: 31,
+			name: 'North Texas Healthcare'
+		}
+	},
+	{
+		id: 5,
+		name: 'AirFit P10 Nasal Pillows CPAP Mask System with Headgear',
+		description: "<p>The AirFit P10 nasal pillows mask is North Texas Healthcare's quietest mask yet. The AirFit P10 retains the proven performance and reliability of the popular Swift FX mask series, but it&rsquo;s&nbsp;approximately 50% quieter and 50% lighter.&nbsp;A recent clinical study found that patients using the AirFit P10 sleep more than 40 minutes longer per night. The new innovative QuietAir woven-mesh venting gently directs away exhaled air, so it's barely noticeable to patients and their bed partners. The AirFit P10 is designed to be as unobtrusive and comfortable as possible, and features a revolutionary new flexi-tube that is ultra light and highly durable.</p><p></p><ul><li>Easy to use - the AirFit P10's minimalist three-piece design makes makes it easy for patients to assemble and clean, promoting acceptance and compliance.</li><li>Easy to fit -&nbsp;QuickFit elastic headgear automatically adjusts to each patient&rsquo;s head and can be fitted in under 30 seconds.</li><li>Easy to assemble - key-clip nasal pillows feature color-coded sizing and left/right-orientation for fast and easy fitting. The mask easily slips on and off without any extra steps.</li><li>Comfortable - the mask seals on contact, with trampoline action that lets patients move around comfortably and freely without compromising the mask&rsquo;s seal.</li></ul>",
+		price: 55.0,
+		currency: USD,
+		in_stock: true,
+		vendor: {
+			id: 29,
+			name: 'North Texas Healthcare'
+		}
+	},
+	{
+		id: 6,
+		name: 'AirFit P10 Nasal Pillows CPAP Mask System with Headgear',
+		description: "<p>The AirFit P10 nasal pillows mask is Cape's quietest mask yet. The AirFit P10 retains the proven performance and reliability of the popular Swift FX mask series, but it&rsquo;s&nbsp;approximately 50% quieter and 50% lighter.&nbsp;A recent clinical study found that patients using the AirFit P10 sleep more than 40 minutes longer per night. The new innovative QuietAir woven-mesh venting gently directs away exhaled air, so it's barely noticeable to patients and their bed partners. The AirFit P10 is designed to be as unobtrusive and comfortable as possible, and features a revolutionary new flexi-tube that is ultra light and highly durable.</p><p></p><ul><li>Easy to use - the AirFit P10's minimalist three-piece design makes makes it easy for patients to assemble and clean, promoting acceptance and compliance.</li><li>Easy to fit -&nbsp;QuickFit elastic headgear automatically adjusts to each patient&rsquo;s head and can be fitted in under 30 seconds.</li><li>Easy to assemble - key-clip nasal pillows feature color-coded sizing and left/right-orientation for fast and easy fitting. The mask easily slips on and off without any extra steps.</li><li>Comfortable - the mask seals on contact, with trampoline action that lets patients move around comfortably and freely without compromising the mask&rsquo;s seal.</li></ul>",
+		price: 5.0,
+		serviceable: true,
+		currency: USD,
+		in_stock: true,
+		vendor: {
+			id: 31,
+			name: 'North Texas Healthcare'
+		}
+	},
+	{
+		id: 7,
+		name: 'AirSense 10 AutoSet CPAP Machine with HumidAir Heated Humidifier and SlimLine Tubing',
+		description: "<p>The New England Medical Supply AirSense 10 AutoSet with HumidAir and SlimLine tubing is a premium pressure-adjusting device with integrated humidifier, cellular connectivity and advanced event detection.</p>",
+		price: 50.0,
+		currency: USD,
+		in_stock: true,
+		vendor: {
+			id: 30,
+			name: 'New England Medical Supply'
+		}
+	},
+	{
+		id: 8,
+		name: 'AirSense 10 AutoSet CPAP Machine with HumidAir Heated Humidifier and SlimLine Tubing',
+		description: "<p>The New England Medical Supply AirSense 10 AutoSet with HumidAir and SlimLine tubing is a premium pressure-adjusting device with integrated humidifier, cellular connectivity and advanced event detection.</p>",
+		price: 45.0,
+		currency: USD,
+		in_stock: true,
+		vendor: {
+			id: 29,
+			name: "New England Medical Supply"
+		}
+	}
+];
